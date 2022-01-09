ce8977c036f2b5dc23dd6e60043afb2f04db32ab
PiYuanZhouLv <18379028718@139.com>
2022-01-09 14:39:21 +0800

    Make setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Enigma',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/PiYuanZhouLv/Enigma',
+    license='',
+    author='PiYuanZhouLv',
+    author_email='18379028718@139.com',
+    description='A Enigma in Python'
+)
